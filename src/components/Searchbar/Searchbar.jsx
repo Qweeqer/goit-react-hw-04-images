f5e17ef5e3ae8a42d1fa5d0120320a5f7cf15eac
Volodymyr Bondarenko <qweeqer2012@gmail.com>
2022-08-22 15:09:13 +0300

    add hooks

--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import { useState } from 'react';
 import PropTypes from 'prop-types';
 import { toast } from 'react-toastify';
 import {
@@ -10,46 +10,42 @@ import {
 
 import s from 'components/Searchbar/Searchbar.module.css';
 
-export class Searchbar extends React.Component {
-  state = {
-    imageSearch: '',
+export function Searchbar({ onSubmit }) {
+  const [imageSearch, setImageSearch] = useState('');
+
+  const handleNameChange = e => {
+    setImageSearch(e.currentTarget.value.toLowerCase());
   };
 
-  handleNameChange = e => {
-    this.setState({ imageSearch: e.currentTarget.value.toLowerCase() });
-  };
-
-  handleSubmit = e => {
+  const handleSubmit = e => {
     e.preventDefault();
-    if (this.state.imageSearch.trim() === '') {
+    if (imageSearch.trim() === '') {
       return toast.error('Please fill in the field!');
     }
-    this.props.onSubmit(this.state.imageSearch);
-    this.setState({ imageSearch: '' });
+    onSubmit(imageSearch);
+    setImageSearch('');
   };
 
-  render() {
-    return (
-      <SearchMenu>
-        <Form className={s.searchForm} onSubmit={this.handleSubmit}>
-          <SearchButton className={s.viewPort} type="submit">
-            Search
-          </SearchButton>
-          <SearchInput
-            className={s.inputSearch}
-            type="text"
-            autoComplete="off"
-            autoFocus
-            placeholder="Search images and photos"
-            name="imageSearch"
-            value={this.state.imageSearch}
-            onChange={this.handleNameChange}
-          />
-          <div className={s.cylonEye}></div>
-        </Form>
-      </SearchMenu>
-    );
-  }
+  return (
+    <SearchMenu>
+      <Form className={s.searchForm} onSubmit={handleSubmit}>
+        <SearchButton className={s.viewPort} type="submit">
+          Search
+        </SearchButton>
+        <SearchInput
+          className={s.inputSearch}
+          type="text"
+          autoComplete="off"
+          autoFocus
+          placeholder="Search images and photos"
+          name="imageSearch"
+          value={imageSearch}
+          onChange={handleNameChange}
+        />
+        <div className={s.cylonEye}></div>
+      </Form>
+    </SearchMenu>
+  );
 }
 
 Searchbar.propTypes = {
